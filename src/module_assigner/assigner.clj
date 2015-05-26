eab7e0e9c935aa0e0de81fb5416ef33c72c3e21e
Mike Bryant <mikesname@gmail.com>
2015-05-26 16:51:41 +0100

    More fleshing out of reassignment logic

--- src/module_assigner/assigner.clj
+++ src/module_assigner/assigner.clj
@@ -11,6 +11,10 @@
 
 (defrecord Student [id name course])
 
+(defrecord Preference [student modules])
+
+(defrecord ModulePreference [module student choice])
+
 (defrecord Assignment
   [module student choice try])
 
@@ -18,7 +22,7 @@
   [modules preferences assignments attempt])
 
 (defn is-favoured [assignment]
-  (= 
+  (=
     (get-in assignment [:module :course])
     (get-in assignment [:student :course])))
 
@@ -38,18 +42,67 @@
   "count the number of students in a module"
   (count (get (by-modules assignments) module)))
 
-(defn move-candidates [assignments]
-  "determine the best assignments to change"
-  (sort-by is-favoured
-    (sort-by :try           
-      (sort-by :choice             
-        (filter (fn [assignment]
-                  (> (module-count assignments (:module assignment)) module-cap))
-                assignments)))))
+(defn over-cap-assignments [assignments]
+  "filter assignments that exist in over-assigned modules"
+  (filter #(> (module-count assignments (:module %)) module-cap)
+          assignments))
 
-(defn assign 
+(defn move-candidates [assignments]
+  """get move candidates (assignments to over-subscribed modules)
+     and order them by priority"""
+  (sort-by
+    (juxt :choice :try is-favoured)
+    (over-cap-assignments assignments)))
+
+(defn next-move-candidate [assignments]
+  "get the highest priority move candidate"
+  (first (move-candidates assignments)))
+
+(defn current-assignments-for [assignments student]
+  "get the assignments that pertain to a given student"
+  (filter #(= student (:student %)) assignments))
+
+(defn preferences-for-student [preferences student]
+  "get the preferences for a given student as a module preference"
+  (let [p (first (filter #(= student (:student %)) preferences))]
+    (let [modules (:modules p)]
+      (map-indexed #(->ModulePreference %2 student %1) modules))))
+
+(defn remaining-preferences [assignments preferences student]
+  "get the remaining preferences for a given student"
+  (let [
+        ;; modules currently assigned for the student
+        current (map #(:module %) (current-assignments-for assignments student))
+        ;; full list of module preferences
+        mod-prefs (preferences-for-student preferences student)]
+    ;; get the prefs to which the student isn't currently assigned
+    (let [rems (into [] (filter
+              (fn [mp] (not (some #(= (:module mp) %) current)))
+            mod-prefs))]
+      (sort-by (juxt :choice #(module-count assignments (:module %)) ) rems))))
+
+(defn next-slot-candidate [assignments preferences assignment]
+  "get the next best empty slot for a to-move assignment"
+  (let [remaining (remaining-preferences assignments preferences (:student assignment))]
+    (first remaining)))
+
+(defn reassign [assignments preferences]
+  "determine how to move someone from an over-cap module to the next best"
+    (let [assign (next-move-candidate assignments)
+          modpref (next-slot-candidate assignments preferences assign)]
+      [assign modpref]))
+
+(defn move-student [assignments preferences assign modpref]
+  "move a student from an over-cap module to the next best"
+  (let [remassignments (into [] (remove #(= assign %) assignments))
+        reassignment (->Assignment (:module modpref) (:student assign) (:choice modpref) (+ 1 (:try assign)))
+       ]
+    (conj remassignments reassignment)))
+
+
+
+(defn assign
   "Given the available modules, a student preferences and the maximum
   and minimum students per module, generate a set of student assignments"
   [modules prefs module-max module-min]
-  
   [])
--- test/module_assigner/assigner_test.clj
+++ test/module_assigner/assigner_test.clj
@@ -35,10 +35,17 @@
   module-xml
   module-python
   module-metadata
-  module-digipres]) 
+  module-digipres])
 
 (def test-preferences [
-                       ])
+  (->Preference student-1 [module-xml module-python module-metadata module-digipres])
+  (->Preference student-2 [module-xml module-python module-metadata module-digipres])
+  (->Preference student-3 [module-xml module-python module-metadata module-digipres])
+  (->Preference student-4 [module-xml module-python module-metadata module-digipres])
+  (->Preference student-5 [module-xml module-python module-metadata module-digipres])
+  (->Preference student-6 [module-xml module-python module-metadata module-digipres])
+  (->Preference student-7 [module-xml module-python module-metadata module-digipres])
+  (->Preference student-8 [module-xml module-python module-metadata module-digipres])])
 
 (def test-assignments [
     (->Assignment module-xml student-1 0 0)
@@ -69,22 +76,89 @@
 (deftest test-move-candidates
   (testing "Move candidates"
     (is (= [] (move-candidates test-assignments)))
-    (let [bad-assignments [
-                           ;; the order here is intentional. Student 5 is
-                           ;; not on the same course as module-xml so is
-                           ;; a higher priority move candiate the students
-                           ;; 1-4.
-                          (->Assignment module-xml student-5 0 0)
-                          (->Assignment module-xml student-4 0 0)
-                          (->Assignment module-xml student-3 0 0)
-                          (->Assignment module-xml student-2 0 0)
-                          (->Assignment module-xml student-1 0 0) ;; more than cap
-                           ]]
-      (is (= bad-assignments (move-candidates bad-assignments))))))
+    (let [ ;; the order here is intentional. Student 5 is
+           ;; not on the same course as module-xml so is
+           ;; a higher priority move candiate the students
+           ;; 1-4.
+           p1 (->Assignment module-xml student-5 0 0)
+           p2 (->Assignment module-xml student-4 1 0)
+           p3 (->Assignment module-xml student-3 2 0)
+           p4 (->Assignment module-xml student-2 3 0)
+           p5 (->Assignment module-xml student-1 4 0)] ;; more than cap
+      (is (= [p1 p2 p3 p4 p5] (move-candidates [p5 p4 p3 p2 p1]))))))
+
+(deftest test-current-assignments-for
+  (testing "Assignments for student"
+    (let [a1 (->Assignment module-xml student-1 0 0)
+          a2 (->Assignment module-python student-1 0 0)
+          a3 (->Assignment module-digipres student-2 0 0)]
+      (is (= [a1 a2] (current-assignments-for [a1 a2 a3] student-1))))))
 
 (deftest test-is-favoured
   (testing "Favoured modules"
     (is (= true (is-favoured (->Assignment module-xml student-1 0, 0))))
     (is (= false (is-favoured (->Assignment module-metadata student-1 0, 0))))))
 
+(deftest test-preferences-for-student
+  (testing "Preferences for student"
+    (let [p1 (->Preference student-1 [module-xml module-python])
+          p2 (->Preference student-2 [module-xml])]
+      (is (=
+            [
+             (->ModulePreference module-xml student-1 0)
+             (->ModulePreference module-python student-1 1)]
+            (preferences-for-student [p1 p2] student-1))))))
+
+(deftest test-remaining-preferences
+  (testing "Remaining preferences for student"
+    (let [p1 (->Preference student-1 [module-xml module-python module-digipres])
+          p2 (->Preference student-2 [module-xml module-python])
+          a1 (->Assignment module-xml student-1 0 0)
+          a2 (->Assignment module-xml student-2 0 0)
+          ]
+      (is (= [
+              (->ModulePreference module-python student-1 1)
+              (->ModulePreference module-digipres student-1 2)
+              ]
+             (remaining-preferences [a1 a2] [p1 p2] student-1)))
+      (is (= [
+              (->ModulePreference module-python student-2 1)
+              ]
+             (remaining-preferences [a1 a2] [p1 p2] student-2))))))
+
+(deftest test-next-slot-candidate
+  (testing "Next slot for an over-cap assignment"
+    (let [p1 (->Preference student-1 [module-xml module-python module-digipres])
+          p2 (->Preference student-2 [module-xml module-python])
+          a1 (->Assignment module-xml student-1 0 0)
+          a2 (->Assignment module-xml student-2 0 0)
+          ]
+      (is (= (->ModulePreference module-python student-1 1)
+             (next-slot-candidate [a1 a2] [p1 p2] a1))))))
+
+(deftest test-reassign-and-move
+  (testing "Reassignment and move"
+    (let [
+         a1 (->Assignment module-xml student-1 0 0)
+         a2 (->Assignment module-xml student-2 0 0)
+         a3 (->Assignment module-xml student-3 0 0)
+         a4 (->Assignment module-xml student-4 0 0)
+         a5 (->Assignment module-xml student-5 0 0)
+         p1 (->Preference student-1 [module-xml module-python])
+         p2 (->Preference student-2 [module-xml module-python])
+         p3 (->Preference student-3 [module-xml module-python])
+         p4 (->Preference student-4 [module-xml module-python])
+         p5 (->Preference student-5 [module-xml module-python])
+          ]
+    (let [[assign modpref] (reassign [a1 a2 a3 a4 a5] [p1 p2 p3 p4 p5])]
+     (is (= student-5 (:student assign)))
+     (is (= 1 (:choice modpref)))
+     (is (= module-python (:module modpref)))
+     (is (= module-xml (:module assign)))
+
+      (let [newassignments (move-student [a1 a2 a3 a4 a5] [p1 p2 p3 p4 p5] assign modpref)
+            assign (->Assignment module-python student-5 1 1)]
+        (is (= 5 (count newassignments)))
+        (is (= assign (last newassignments)))
+        )))))
 
