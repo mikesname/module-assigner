0d11807e03ca1b31c14b4afec40788aeebffeed3
Mike Bryant <mikesname@gmail.com>
2015-07-06 13:57:21 +0100

    Combine steps 1 & 2 into a single form

--- src/module_assigner/handler.clj
+++ src/module_assigner/handler.clj
@@ -22,7 +22,7 @@
 
 (defsnippet step1-t "module-assigner/step1.html" [:div.article] [] identity)
 
-(defsnippet module-snippet "module-assigner/step2.html" 
+(defsnippet module-snippet "module-assigner/results.html" 
   [:#modules :tbody [:tr first-of-type]]
   [modules]
   [:tr] (clone-for [module modules]
@@ -31,7 +31,7 @@
                    [:tr [:td (nth-of-type 3)]] (content (str (:term module)))
                    [:tr [:td (nth-of-type 4)]] (content (:name (:course module)))))
 
-(defsnippet preferences-snippet "module-assigner/step3.html" 
+(defsnippet preferences-snippet "module-assigner/results.html" 
   [:#preferences :tbody [:tr first-of-type]]
   [assignments cap]
   [:tr] (clone-for [[module students] (by-modules assignments)]
@@ -42,7 +42,7 @@
                                                    ""))
                    [:tr [:td (nth-of-type 3)]] (content (clojure.string/join ", " (map :name students)))))
 
-(defsnippet solved-assignments-snippet "module-assigner/step3.html" 
+(defsnippet solved-assignments-snippet "module-assigner/results.html" 
   [:#assignments :tbody [:tr first-of-type]]
   [assignments]
   [:tr] (clone-for [[module students] (by-modules assignments)]
@@ -50,7 +50,7 @@
                    [:tr [:td (nth-of-type 2)]] (content (str (count students)))
                    [:tr [:td (nth-of-type 3)]] (content (clojure.string/join ", " (map :name students)))))
 
-(defsnippet moves-snippet "module-assigner/step3.html" 
+(defsnippet moves-snippet "module-assigner/results.html" 
   [:#moves :tbody [:tr first-of-type]]
   [moves]
   [:tr] (clone-for [move moves]
@@ -58,17 +58,7 @@
                    [:tr [:td (nth-of-type 2)]] (content (:name (:from move)))
                    [:tr [:td (nth-of-type 3)]] (content (:name (:to move)))))
 
-(defsnippet step2-t "module-assigner/step2.html" [:form] [modules modcsv cap]
-  [:input#module-cap] (set-attr :value cap)
-  [:input#module-data] (set-attr :value modcsv)
-  [:#modules :tbody] (content (module-snippet modules)))
-
-(defsnippet step2-error "module-assigner/step1.html" [:form] [error cap]
-  [:input#module-cap] (set-attr :value cap)
-  [:.modules-file] (add-class "error")
-  [:.modules-file :.form-errors] (content error))
-
-(defsnippet step3-t "module-assigner/step3.html" [:form] [modules modcsv prefs prefcsv solved cap]
+(defsnippet results-t "module-assigner/results.html" [:form] [modules modcsv prefs prefcsv solved cap]
   [:input#module-cap] (set-attr :value cap)
   [:input#module-data] (set-attr :value modcsv)
   [:input#preference-data] (set-attr :value prefcsv)
@@ -81,10 +71,8 @@
                              (:assignments solved)))
   [:#moves :tbody] (content (moves-snippet (:moves solved))))
 
-(defsnippet step3-error "module-assigner/step2.html" [:form] [modules modcsv error cap]
+(defsnippet results-error "module-assigner/step1.html" [:form] [error cap]
   [:input#module-cap] (set-attr :value cap)
-  [:input#module-data] (set-attr :value modcsv)
-  [:#modules :tbody] (content (module-snippet modules))
   [:.form-errors] (content error))
 
 
@@ -95,31 +83,26 @@
   (page-t title t))
 
 (defn render-err [title t]
-  {:body (page-t title t) :status 400})
+  {:body (render-page title t) :status 400})
 
-(defn upload-modules
-  [file filename cap]
-  (if (not (clojure.string/blank? filename))
-    (try
-      (let [csvstr (slurp file) modules (read-modules csvstr)]
-        (render-page "Step 2" (step2-t modules csvstr cap)))
-      (catch clojure.lang.ExceptionInfo e
-        (render-err "Data Error" (step2-error (.getMessage e)))))
-    (render-err "Error" (step2-error "no file given" cap))))
-
-(defn upload-preferences
-  [file filename modcsv cap]
-  (let [modules (read-modules modcsv)]
-    (if (not (clojure.string/blank? filename))
+(defn upload-data
+  [modfile modfilename preffile preffilename cap]
+  (cond
+    (nil? modfile)
+      (render-err "Error" (results-error "No module file given" cap))
+    (nil? preffile)
+      (render-err "Error" (results-error "No preferences file given" cap))
+    :else      
       (try    
-        (let [prefcsv (slurp file)
+        (let [modcsv (slurp modfile)
+              prefcsv (slurp preffile)
+              modules (read-modules modcsv)
               prefs (read-preferences modules prefcsv)
               solved (calculate-terms modules prefs cap (range 1 3))]
           (print-report solved)
-          (render-page "Result" (step3-t modules modcsv prefs prefcsv solved cap)))
+          (render-page "Result" (results-t modules modcsv prefs prefcsv solved cap)))
         (catch clojure.lang.ExceptionInfo e
-          (render-err "Error" (step3-error modules modcsv (.getMessage e) cap))))
-      (render-err "Error" (step3-error modules modcsv "no file given" cap)))))
+          (render-err "Error" (results-error (.getMessage e) cap))))))
 
 (defn send-result-csv [modcsv prefcsv cap]
   (let [modules (read-modules modcsv)
@@ -134,20 +117,14 @@
 (defroutes app-routes
   (GET "/" [] (render-page "Module Assigner" (step1-t)))
 
-  (POST "/step2" {:keys [params]}
-        (let [{:keys [tempfile filename]} (get params :file)
+  (POST "/assign" {:keys [params]}
+        (let [{modfile :tempfile modfilename :filename} (get params :modfile)
+              {preffile :tempfile preffilename :filename} (get params :preffile)              
               cap (module-cap-with-default (get params :modcap))]
-          (upload-modules tempfile filename cap)))
+          (upload-data modfile modfilename preffile preffilename cap)))
   
-  (POST "/step3" {:keys [params]}
-        (let [{:keys [tempfile filename]} (get params :file)
-              modcsv (get params :modcsv)
-              cap (module-cap-with-default (get params :modcap))]
-          (upload-preferences tempfile filename modcsv cap)))
-
   (POST "/download" {:keys [params]}
-        (let [
-              prefcsv (get params :prefcsv)
+        (let [prefcsv (get params :prefcsv)
               modcsv (get params :modcsv)
               cap (module-cap-with-default (get params :modcap))]
           (send-result-csv modcsv prefcsv cap)))
--- test/module_assigner/handler_test.clj
+++ test/module_assigner/handler_test.clj
@@ -10,16 +10,17 @@
 
 (def module-cap "10")
 
-(defn- build-multipart [params file]
+(defn- build-multipart [params files]
   (let [builder (MultipartEntityBuilder/create)]
     (doseq [[key value] params]
       (.addTextBody builder (name key) value))
-    (.addBinaryBody
-      builder
-      (:name file)
-      (.getBytes (:data file) "UTF-8")
-      ContentType/TEXT_PLAIN
-      (:filename file))
+    (doseq [[key file] files]
+      (.addBinaryBody
+        builder
+        (name key)
+        (.getBytes (:data file) "UTF-8")
+        ContentType/TEXT_PLAIN
+        (:filename file)))
     (.setBoundary builder "12345678")
     (let [writer (new ByteArrayOutputStream)]
       (.writeTo (.build builder) writer)
@@ -31,35 +32,27 @@
       (is (= (:status response) 200))
       (is (.contains (apply str (:body response)) "Step 1"))))
 
-  (testing "step 1"
+  (testing "assignment"
     (let [module-data (slurp (-> "modules.csv" io/resource io/file))
-          response (app (-> (mock/request :post "/step2" (build-multipart {:modcap module-cap} {
-                                                                             :name "file"
-                                                                             :filename "modules.csv"
-                                                                             :data module-data}))
+          preference-data (slurp (-> "preferences.csv" io/resource io/file))
+          response (app (-> (mock/request :post "/assign" (build-multipart {:modcap module-cap} {                                                                                                             :modfile {
+                                                                               :filename "modules.csv"                                                                                                           :data module-data
+                                                                             }
+                                                                             :preffile {
+                                                                               :filename "preferences.csv"
+                                                                               :data preference-data         
+                                                                             }}))
                             (mock/content-type "multipart/form-data; boundary=12345678")))]
       (is (= (:status response) 200))
-      (is (.contains (apply str (:body response)) "Step 2"))))
+      (is (.contains (apply str (:body response)) "Student preferences"))
+      (is (.contains (apply str (:body response)) "Calculated assignments"))))
 
-  (testing "step 1 with no file"
-    (let [response (app (-> (mock/request :post "/step2")))]
+  (testing "process with no file"
+    (let [response (app (-> (mock/request :post "/assign")))]
       (is (= (:status response) 400))
       (is (.contains (apply str (:body response)) "Step 1"))))
-
-  (testing "step 2"
-    (let [
-          module-data (slurp (-> "modules.csv" io/resource io/file))
-          preferences-data (slurp (-> "preferences.csv" io/resource io/file))
-          mp (build-multipart {:modcsv module-data :modcap module-cap} {
-                             :name "file"
-                             :filename "preferences.csv"
-                             :data preferences-data})]
-      (let [
-          response (app (-> (mock/request :post "/step3" mp)
-                            (mock/content-type "multipart/form-data; boundary=12345678")))]
-        (is (= (:status response) 200))
-        (is (.contains (apply str (:body response)) "Calculated assignments")))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
